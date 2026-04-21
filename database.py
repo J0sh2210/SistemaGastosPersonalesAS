6d1518425fcb2357497e656e8fac923efc2ba6ad
GussRamirez0 <lramirezb10@miumg.edu.gt>
2026-04-20 23:31:06 -0600

    chore: sync local files with remote

---
+++ database.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+load_dotenv()
+
+cadena = os.getenv("DB_CadenaConection")
+SQLALCHEMY_DATABASE_URL = cadena
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
---
+++ services/auth_service.py
@@ -0,0 +1,47 @@
+from passlib.context import CryptContext
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+# Encriptación
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+# JWT config
+SECRET_KEY = "mi_clave_secreta_super_segura"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+# Para proteger rutas
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="usuarios/login")
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+
+        if username is None:
+            raise HTTPException(status_code=401, detail="Token inválido")
+
+        return username
+
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Token inválido")
