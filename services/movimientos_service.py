da749c5884720e7463f7138202bdd48c87ebb1e2
Josseline-g <jgaleanoh@miumg.edu.gt>
2026-04-27 11:09:24 -0600

    PFA-29: Operación entre movimientos

--- services/movimientos_service.py
+++ services/movimientos_service.py
@@ -40,3 +40,66 @@ def editar_categoria_movimiento(idMovimiento: int, idCategoria: int):
 
     finally:
         db.close()
+
+def calcular_diferencia(tipo: str):
+    db = SessionLocal()
+
+    if tipo == "mes":
+        query = text("""
+            SELECT 
+                YEAR(m.FechaMovimiento) AS Anio,
+                MONTH(m.FechaMovimiento) AS Mes,
+                SUM(
+                    CASE 
+                        WHEN tm.Nombre = 'Ingreso' THEN m.Monto
+                        WHEN tm.Nombre = 'Egreso' THEN -m.Monto
+                        ELSE 0
+                    END
+                ) AS Total
+            FROM Movimiento m
+            INNER JOIN TipoMovimiento tm 
+                ON m.IdTipo = tm.IdTipo
+            GROUP BY 
+                YEAR(m.FechaMovimiento),
+                MONTH(m.FechaMovimiento)
+            ORDER BY 
+                Anio, Mes
+        """)
+    else:  # año
+        query = text("""
+            SELECT 
+                YEAR(m.FechaMovimiento) AS Anio,
+                SUM(
+                    CASE 
+                        WHEN tm.Nombre = 'Ingreso' THEN m.Monto
+                        WHEN tm.Nombre = 'Egreso' THEN -m.Monto
+                        ELSE 0
+                    END
+                ) AS Total
+            FROM Movimiento m
+            INNER JOIN TipoMovimiento tm 
+                ON m.IdTipo = tm.IdTipo
+            GROUP BY 
+                YEAR(m.FechaMovimiento)
+            ORDER BY 
+                Anio
+        """)
+
+    resultado = db.execute(query).fetchall()
+    db.close()
+
+    data = []
+    for row in resultado:
+        if tipo == "mes":
+            data.append({
+                "anio": row.Anio,
+                "mes": row.Mes,
+                "total": float(row.Total)
+            })
+        else:
+            data.append({
+                "anio": row.Anio,
+                "total": float(row.Total)
+            })
+
+    return data
